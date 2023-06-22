d020734c2b13aca842076fb00552f51c47c902e6
Guilherme Fernando de Souza <guilherme_h14@hotmail.com>
2023-06-22 19:49:28 -0300

    correcao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,57 @@
+import { Foto } from './../models/Foto.interface';
+import { FotoService } from './../services/foto.service';
 import { Component } from '@angular/core';
-import { FotoService } from '../services/foto.service';
+import { ActionSheetController, ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements ViewWillEnter {
+  fotos: Foto[] = [];
 
-  constructor(public fotoService: FotoService) {}
+  constructor(
+    private fotoService: FotoService,
+    private actionSheetController: ActionSheetController
+  ) {}
+
+  ionViewWillEnter() {
+    this.getFotos();
+  }
+
+  async ngOnInit() {
+    await this.fotoService.carregarFotosSalvas();
+  }
 
   tirarFoto() {
     this.fotoService.tirarFoto();
   }
 
+  public async showActionSheet(foto: Foto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Fotos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.fotoService.deletePicture(foto, position);
+        }
+      },
+      {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+
+      }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  getFotos() {
+    this.fotos = this.fotoService.fotos;
+  }
 }
